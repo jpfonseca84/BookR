1ddd9c14bc68725f1937ea62a06be6d24c5cf0a3
jpfonseca84 <jpfonseca@gmail.com>
2017-02-17 11:20:18 -0500

    Exercise completed and Reviewed

--- Chapter 21/Chapter 21 Exercises 3.R
+++ Chapter 21/Chapter 21 Exercises 3.R
@@ -10,7 +10,8 @@ summary(cat.fit)
 
 #The significance of the Bwt had decreased, ut the remaining variables 
 #significance had increased. ALso, Bwt Main interaction had decreased compared
-# with the new model with interactive interactions.
+# with the new model with interactive interactions. Also, being a male had a 
+#slight positive interative effect on Bwt.
 
 #b ----
 plot(cats$Hwt~cats$Bwt,
@@ -28,32 +29,30 @@ cat.fema<-c(coef(cat.fit)[1],coef(cat.fit)[2])
 abline(cat.male,col="blue")
 abline(cat.fema,col="red")
 
-#Now, the interactive interactions are impacting each other, being a Female or 
-#being a Male have impact on the regressions HeartWeight. Previously, using only 
-#the Main effects, no great difference between sex was identified.
+#Now, the interactive interactions are impacting each other, being a Female 
+#or being a Male have impact on the regressions HeartWeight. Previously, 
+#using only the Main effects, no great difference between sex was identified.
 
 #c ----
-interactive.predict<-predict(cat.fit,
-        newdata = data.frame(Bwt=3.34,Sex="F"),
+predict(cat.fit, #interactive predict
+        newdata = data.frame(Bwt=3.4,Sex="F"),
         interval="prediction")
-main.predict<-predict(cat.original.fit,
-                      newdata = data.frame(Bwt=3.34,Sex="F"),
+predict(cat.original.fit,#main predict
+                      newdata = data.frame(Bwt=3.4,Sex="F"),
                       interval="prediction")
 
-#The new model, with interactive effects can show the impact of being a female
-#has in the overall weight.
+#The new model, with interactive effects can show the impact of being a 
+#female has in the overall weight, bringing a lighter weight than predicted 
+#previously
 
-#Second Part 
-#d
+#Second Part ----
 
-
-#Second Part
 #d ----
 library(faraway)
 ?trees
 
 tree.mainfit<-lm(Volume~Girth+Height,data=trees)
-summary(tree.fit)
+summary(tree.mainfit)
 tree.intfit<-lm(Volume~Girth*Height,data=trees)
 summary(tree.intfit)
 
@@ -61,24 +60,38 @@ summary(tree.intfit)
 #the variables. Still, statistically acceptable 
 
 #e ----
+tree.log.mainfit<-lm(log(Volume)~log(Girth)+log(Height),data=trees)
+summary(tree.log.mainfit)
 tree.log.intfit<-lm(log(Volume)~log(Girth)*log(Height),data=trees)
 summary(tree.log.intfit)
 #we got an even greater Rsquared value, but now, all coefficients have huge
 # significane levels, not acceptable. We cannot assume they have a log
 # relation
 
-
-
-#Third Part
+#Third Part ----
 #f ----
 ?mtcars
-cars.fit<-lm(mpg~hp*cyl,data=mtcars)
+cars.fit<-lm(mpg~hp*factor(cyl)+wt,data=mtcars)
 summary(cars.fit)
 
 #g ----
-#the summary for the car.fit presents the model as statistical relevant with 
-#it's R-Squared value and also, presents good p-values for the variables 
-#themselves. The interaction between hp and Cyl is still relevant, but it has 
-#low impact in the regression.
+#The hp variable is a continuous and the factor(cyl) is a categorical. Each of 
+#'their interactions can be identified as a change in the slope of the 
+#'model.
 
-#h
+#h ----
+#mom wants mpg>=25 
+mycars<-data.frame(cyl=c(4,8,6),
+                   hp=c(100,210,200),
+                   wt=c(2.1,3.9,2.0),
+                   row.names = c("Car1","Car2","Car3"))
+
+predict(cars.fit,newdata = mycars,interval="confidence")
+
+#i
+#My predictive model suggest that the only car to achieve the desired MPG is
+#car1
+
+#ii 
+#The regression includes for the car3 a upper limit that includes 25Mpg. 
+#There is 95% of confidence the average for that car is between those values
