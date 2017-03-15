5b90a78c4cb7c5fa963761d95951c98762fd8626
jpfonseca84 <jpfonseca@gmail.com>
2017-03-15 16:46:12 -0400

    indent adjustment

--- Book of R answers/Davies_Part4_Solutions.R
+++ Book of R answers/Davies_Part4_Solutions.R
@@ -832,28 +832,42 @@ predict(car.fit,
 #(a)
 library("boot")
 nuc.null <- lm(cost~1,data=nuclear)
-nuc.step <- step(nuc.null,scope=.~.+date+t1+t2+cap+pr+ne+ct+bw+cum.n+pt,direction="both")
+nuc.step <- step(nuc.null,scope=.~.+date+t1+t2+cap+pr+ne+ct+bw+cum.n+pt,
+                 direction="both")
 summary(nuc.step)
-#(b) The model in (a) is again different to that selected by either forward or backward elimination. It's most similar to the model chosen via backward selection, differing only by an additional, non-significant, effect for 'ct'.
+#(b) The model in (a) is again different to that selected by either 
+#forward or backward elimination. It's most similar to the model chosen
+#via backward selection, differing only by an additional, non-significant, 
+#effect for 'ct'.
+
 #(c)
-gal <- data.frame(d=c(573,534,495,451,395,337,253),h=c(1,0.8,0.6,0.45,0.3,0.2,0.1))
+gal <- data.frame(d=c(573,534,495,451,395,337,253),
+                  h=c(1,0.8,0.6,0.45,0.3,0.2,0.1))
 gal.mod0 <- lm(d~1,data=gal)
 gal.mod1 <- lm(d~h,data=gal)
 gal.mod2 <- lm(d~h+I(h^2),data=gal)
 gal.mod3 <- lm(d~h+I(h^2)+I(h^3),data=gal)
 gal.mod4 <- lm(d~h+I(h^2)+I(h^3)+I(h^4),data=gal)
+
 #(d)
 anova(gal.mod0,gal.mod1,gal.mod2,gal.mod3,gal.mod4)
-# Yes, the model chosen via nested partial F-tests matches the cubic model you would have likely identified in Exercise 21.2. The improvement to goodness-of-fit is minimal (and not statistically significant) if moving from the order 3 to the order 4 model.
+# Yes, the model chosen via nested partial F-tests matches the cubic 
+# model you would have likely identified in Exercise 21.2. The 
+#improvement to goodness-of-fit is minimal (and not statistically 
+#significant) if moving from the order 3 to the order 4 model.
+
 #(e)
 library("faraway")
 diab <- na.omit(diabetes[,c("chol","age","gender","height","weight","frame","waist","hip","location")])
+
 #(f)
 dia.null <- lm(chol~1,data=diab)
 dia.full <- lm(chol~age*gender*weight*frame+waist*height*hip+location,data=diab)
+
 #(g)
 dia.step <- step(dia.null,scope=.~.+age*gender*weight*frame+waist*height*hip+location)
 summary(dia.step)
+
 #(h)
 add1(dia.null,scope=.~.+age*gender*weight*frame+waist*height*hip+location,test="F")
 dia.1 <- update(dia.null,.~.+age)
@@ -861,17 +875,40 @@ add1(dia.1,scope=.~.+age*gender*weight*frame+waist*height*hip+location,test="F")
 dia.2 <- update(dia.1,.~.+frame)
 add1(dia.2,scope=.~.+age*gender*weight*frame+waist*height*hip+location,test="F")
 summary(dia.2)
-# The two models in (g) and (h) are quite different. Step-wise AIC has chosen a model that includes the two-way interaction between 'age' and 'frame', as well as a main effect for 'waist'. Performing forward selection halts after main effects for 'age' and 'frame' are added only, with no further additions offering a statistically significant improvement to goodness-of-fit.
+# The two models in (g) and (h) are quite different. Step-wise AIC has 
+#chosen a model that includes the two-way interaction between 'age' 
+#and 'frame', as well as a main effect for 'waist'. Performing forward 
+#selection halts after main effects for 'age' and 'frame' are added only,
+#with no further additions offering a statistically significant 
+#improvement to goodness-of-fit.
+
 #(i)
 dia.step2 <- step(dia.full)
 summary(dia.step2)
-# The model chosen via step-wise AIC when starting from 'dia.full' is far, far more complex than the AIC-selected model in (g). The model contains a three-way interaction between 'age', 'gender' and 'weight', and all lower-order effects, as well as main effects and a two-way interaction between 'waist' and 'hip'. By starting from the very complex model, it seems we've accessed potential candidate models which have AIC values that are lower than those accessible by starting the selection algorithm at the null (intercept-only) model. For this reason, the final model reached in this way refuses to move to the simpler model selected in (g) since it's already found a model with a lower value for that criterion. Whether or not we'd be willing to settle on this rather complex model for predictive purposes is a more difficult question to answer...
+# The model chosen via step-wise AIC when starting from 'dia.full' is 
+# far, far more complex than the AIC-selected model in (g). The model 
+#contains a three-way interaction between 'age', 'gender' and 'weight',
+#and all lower-order effects, as well as main effects and a two-way 
+#interaction between 'waist' and 'hip'. By starting from the very 
+#complex model, it seems we've accessed potential candidate models 
+#which have AIC values that are lower than those accessible by 
+#starting the selection algorithm at the null (intercept-only) model. 
+#For this reason, the final model reached in this way refuses to move 
+#to the simpler model selected in (g) since it's already found a model 
+#with a lower value for that criterion. Whether or not we'd be willing 
+#to settle on this rather complex model for predictive purposes is a 
+#more difficult question to answer...
+
 #(j)
 library("MASS")
 car.null <- lm(I(1/mpg)~1,data=mtcars)
-car.step <- step(car.null,scope=.~.+wt*hp*factor(cyl)*disp+am+factor(gear)+drat+vs+qsec+carb)
+car.step <- step(car.null,scope=.~.+wt*hp*factor(cyl)*disp+am+factor(gear)+
+                       drat+vs+qsec+carb)
 summary(car.step)
-# Modeling the response as GPM instead of MPG, step-wise AIC selection offers up a far simpler model, with main effects for 'wt' and 'hp' only. It would seem that this transformation of the response greatly simplifies the relationships in the data.
+# Modeling the response as GPM instead of MPG, step-wise AIC selection 
+#offers up a far simpler model, with main effects for 'wt' and 'hp' only.
+#It would seem that this transformation of the response greatly simplifies 
+#the relationships in the data.
 
 ## 22.2 ####
 #(a)
@@ -915,8 +952,14 @@ plot(dia.fit,which=5,cook.levels=c(1,3,5)*cutoff)
 dia.url <- "http://www.amstat.org/publications/jse/v9n2/4cdata.txt"
 diamonds <- read.table(dia.url)
 names(diamonds) <- c("Carat","Color","Clarity","Cert","Price")
-plot(diamonds$Carat,diamonds$Price,pch=19,col=as.numeric(diamonds$Clarity))
-legend("topleft",legend=levels(diamonds$Clarity),col=1:length(levels(diamonds$Clarity)),pch=19)
+plot(diamonds$Carat,
+     diamonds$Price,
+     pch=19,
+     col=as.numeric(diamonds$Clarity))
+legend("topleft",
+       legend=levels(diamonds$Clarity),
+       col=1:length(levels(diamonds$Clarity)),
+       pch=19)
 plot(diamonds$Carat,diamonds$Price,pch=19,col=as.numeric(diamonds$Color))
 legend("topleft",legend=levels(diamonds$Color),col=1:length(levels(diamonds$Color)),pch=19)
 plot(diamonds$Carat,diamonds$Price,pch=19,col=as.numeric(diamonds$Cert))
